ba94e4d6dc65f51be1aac7ef7105693fad57a618
Nikita Trophimov <n.trophimov@tools4brokers.com>
2016-04-18 11:26:16 +0400

    Init

---
+++ script.js
@@ -0,0 +1,300 @@
+// * This is a test script that deomnstrates some abilities of the Hafen's client scripting
+//
+// It does the following actions:
+// - Logs in using provided login and password (replace them with the actual ones in `onLogin` function below)
+// - Selects a specified character (replace it with the real one in `onCharSelect` function below)
+// - Starts autowalking (with pathfinding)
+// - When a forageable curio encountered, it picks it and place it in the study interface if it is a dandelion
+// - When an aggresive animal or an unknown player encountered, it teleports to the hearth fire and starts autowalking again
+//
+// There are a lot more functions that you can use for the client scripting, please see the comments below
+//
+// * You should use a patched version of Amber client to make it work -- https://www.dropbox.com/s/srykok9g0uljfk6/amber-script.zip?dl=0
+// Source code is available here -- https://github.com/b0r3d0m/amber ("feature/script-api" branch)
+//
+// * This script should be named "script.js" (w/o double quotes) and placed in the root directory of the modified Amber client (the same directory where you have the "hafen.jar" file)
+//
+// * It is written in Javascript but you can use Java standard library via `java` object (for example, `java.lang.Thread.sleep`)
+//
+// * Please read all NOTEs and comments
+//
+// * I'm strongly recommend not to use the game's client by yourself when there's "script.js" file in the game's directory
+//   If you want to use the client by yourself, rename this script to something else and restart the client
+//
+// * Feel free to ask me any questions
+//
+// b0r3d0m
+
+///////////////////////
+// User-defined "global" variables
+///////////////////////
+
+var g = null;
+var autoWalkingTimerId = null;
+
+///////////////////////
+// API events
+///////////////////////
+
+// NOTE that you should define all API event functions, even if you don't want to handle some of them
+//      Just do nothing if you don't want to process such events
+
+// It is fired when the login screen appeared
+// You should return an object with the `login` and `password` properties set to the appropriate values
+function onLogin() {
+  print('Logging in...');
+
+  var credentials = {
+    login: 'login',
+    password: 'pass'
+  };
+  return credentials;
+}
+
+// It is fired 3 seconds after the character select. screen appeared
+// You should return a name of the character you want to select
+function onCharSelect() {
+  print('Selecting character...');
+
+  return 'somechar';
+}
+
+// It is fired 10 seconds after the selected character entered the world
+// `game` is the main interface to the client API, so generally you should keep a reference to it somewhere in the script
+function onGameLoaded(game) {
+  print('Game loaded');
+
+  g = game;
+
+  print('Starting autowalking...');
+  startAutoWalking();
+}
+
+// `game` interface:
+// - getPlayerCoords()
+//   Returns an object with the `x` and `y` properties represents the player's coordinates
+//
+// - getInvItems()
+//   Returns an array of items' names from the character's inventory or `null` if there was an error
+// 
+// - studyCurio(curioName)
+//   Places a curio with the specified name from the character's inventory to the study inventory
+//   Returns `true` on success and `false` otherwise
+//   NOTE that it doesn't handle situations when you don't have enough points to study the specified curio (it returns `true` in such cases)
+//        Use function `getCharAttentionInfo` to check it
+//
+// - getCharAttentionInfo()
+//   Returns an object with `max` and `used` properties 
+// 
+// - goTo(x, y)
+//   Makes the character go to the specified coordinates (with the pathfinding enabled)
+// 
+// - pickItem(id)
+//   Picks a foragable item with the specified id (with the pathfinding enabled)
+//   Takes item's id
+//   Returns `true` if there is an item with such id or `false` otherwise
+// 
+// - travelToHearthFire()
+//   Makes the character travel to its hearth fire
+//
+// - getMapObjects(name)
+//   Returns an array of objects with `id` and `coords` properties
+//
+// - mapObjectRightClick(id)
+//   Clicks an object with the specified ID via the right mouse button
+//   Returns `true` on success or `false` otherwise
+//
+// - transferItem(name)
+//   Transfers an item with the specified name from the character's inventory to an opened container (or study interface if there are no containers opened)
+//   Returns `true` on success or `false` otherwise
+//
+// - transferItems(name)
+//   The same as the `transferItem` function but transfers all objects wit the specified name
+//   Returns `true` on success or `false` otherwise
+//
+// - getItemsFrom(windowName)
+//   Returns an array of items' names from the specified window (for example, 'Frame' in case of drying frames) or `null` if there was an error
+//
+// - transferItemFrom(windowName, itemName)
+//   Transfers an item with the specified name from the specified window to the character's inventory
+//   Returns `true` on success or `false` otherwise
+//
+// - transferItemsFrom(windowName, itemName)
+//   The same as `transferItemFrom` function but transfers all objects with the specified name
+//   Returns `true` on success or `false` otherwise
+//
+// - getStamina()
+//   Returns stamina of the player or -1 if there was an error
+//
+// - drink()
+//   Makes the character drink water from any of the following containers -- bucket, kuksa, waterskin and waterflask (from both character's inventory and equipment screen)
+//   NOTE that this method, unlike many others, is synchronous
+//        This is a result of its "adaptive" behavior -- it tries to drink from one container and if it's not enough it switches to the next one
+// 
+// - logout()
+//   Logs out from the current account
+// 
+// - quit()
+//   Closes game's window
+//   NOTE that in most situations it's better to call the `logout` function instead of this one
+//        because the `logout` sends a direct message about your willings to the game's server while the `quit` function
+//        just simply calls `System.exit(0)` that can lead to the more "delayed" logout
+
+// NOTE that any events except `onLogin` and `onCharSelect` can be fired **before** `onGameLoaded`
+// NOTE that the majority of API functions are asynchronous -- including but not limited to `goTo`, `pickItem`, `travelToHearthFire` and `logout`
+//      Synrhonous ones include `getInvItems` and `quit`
+// NOTE that you should NOT use the === operator when the API objects take place in comparisons, use == operator instead
+// NOTE that any functions that work with items' names expect an item's base resource name
+//      So instead of 'Dried Fox Hide' you should pass 'foxhide', instead of 'Raw Fox Hide' you should pass 'foxhide-blood' etc
+
+// It is fired when a foragable curio was found (once for each of them)
+function onCurioFound(id, name, coords) {
+  if (g == null) {
+    return;
+  }
+
+  print('Curio "' + name + '" found, trying to pick it...');
+
+  stopAutoWalking();
+  g.pickItem(id);
+
+  setTimeout(function() {
+    if (name == 'dandelion') {
+      var mw = getMentalWeight(name);
+
+      var attentionInfo = g.getCharAttentionInfo();
+      var freeAttention = attentionInfo.max - attentionInfo.used;
+      if (freeAttention >= mw) {
+        print('Trying to study the curio...');
+        g.studyCurio(name);
+      }
+    }
+
+    print('Resuming autowalking...');
+    startAutoWalking();
+  }, 10000);
+}
+
+// It is fired when a creature was encountered (once for each of them)
+function onCreatureFound(id, name, coords) {
+  if (g == null) {
+    return;
+  }
+
+  if (isAggr(name)) {
+    print('Creature "' + name + '" encountered, trying to travel to the hearth fire...');
+
+    stopAutoWalking();
+    g.travelToHearthFire();
+
+    setTimeout(function() {
+      print('Resuming autowalking...');
+      startAutoWalking();
+    }, 10000);
+  }
+}
+
+// It is fired when another player was encountered (once for each of them)
+function onPlayerFound(id, isKin, coords) {
+  if (g == null) {
+    return;
+  }
+
+  if (!isKin) {
+    print('Non-kin player encountered, trying to travel to the hearth fire...');
+
+    stopAutoWalking();
+    g.travelToHearthFire();
+
+    setTimeout(function() {
+      print('Resuming autowalking...');
+      startAutoWalking();
+    }, 10000);
+  }
+}
+
+///////////////////////
+// User-defined functions
+///////////////////////
+
+// Starts autowalking timer that will move character x+50, y+50 every second
+function startAutoWalking() {
+  autoWalkingTimerId = setInterval(function() {
+    var playerCoords = g.getPlayerCoords();
+    g.goTo(playerCoords.x + 50, playerCoords.y + 50);
+  }, 1000);
+}
+
+// Stops autowalking timer
+function stopAutoWalking() {
+  clearInterval(autoWalkingTimerId);
+  // NOTE that clearInterval (nor clearTimeout) does NOT wait for the currently executing task to finish
+  //      so let's sleep for some time manually
+  sleep(2000);
+}
+
+// Determines whether the specified animal is aggresive (NOTE that this function is incomplete)
+function isAggr(animal) {
+  return animal == "badger" || animal == "lynx" || animal == "bat" || animal == "bear" || animal == "boar"; // etc
+}
+
+// Returns a mental weight of the specified curio (NOTE that this function is incomplete)
+function getMentalWeight(curioName) {
+  var mws = {
+    'dandelion': 1
+    // etc
+  };
+  return mws[curioName];
+}
+
+// Prints the specified message to stdout
+function print(msg) {
+  java.lang.System.out.println(msg);
+}
+
+// Sleeps for the specified amount of milliseconds
+// NOTE that this function should be used carefully
+//      It prevents the event loop from triggering new actions until you finally return from the API event function
+//      If possible, use `setTimeout` and `setInterval` functions instead
+function sleep(millisecs) {
+  java.lang.Thread.sleep(millisecs);
+}
+
+///////////////////////
+// Helper functions to work with timeouts and intervals
+///////////////////////
+
+var setTimeout,
+    clearTimeout,
+    setInterval,
+    clearInterval;
+
+(function () {
+    var timer = new java.util.Timer();
+    var counter = 1; 
+    var ids = {};
+
+    setTimeout = function(fn, delay) {
+        var id = counter++;
+        ids[id] = new JavaAdapter(java.util.TimerTask, {run: fn});
+        timer.schedule(ids[id], delay);
+        return id;
+    }
+
+    clearTimeout = function(id) {
+        ids[id].cancel();
+        timer.purge();
+        delete ids[id];
+    }
+
+    setInterval = function(fn, delay) {
+        var id = counter++; 
+        ids[id] = new JavaAdapter(java.util.TimerTask, {run: fn});
+        timer.schedule(ids[id], delay, delay);
+        return id;
+    }
+
+    clearInterval = clearTimeout;
+
+})();
+
